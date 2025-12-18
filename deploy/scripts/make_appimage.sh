a6e1e8f93ee550f2c2816c6268e0df7f84040205
blackwille <willem_black@vk.com>
2025-12-18 16:49:19 +0300

    Renamed output linux file in make_appimage.sh

--- deploy/scripts/make_appimage.sh
+++ deploy/scripts/make_appimage.sh
@@ -2,7 +2,7 @@
 
 LINUXDEPLOY_DIR="${HOME}/MyApps/AppImage/linuxdeploy-x86_64.AppImage"
 DEPLOY_DIR="$(dirname $(dirname $(realpath $0)))/linux/"
-export LDAI_OUTPUT="${DEPLOY_DIR}/Zvonilka_x86_64.AppImage"
+export LDAI_OUTPUT="${DEPLOY_DIR}/Zvonilka_linux_x86_64.AppImage"
 LD_LIBRARY_PATH=${DEPLOY_DIR}/usr/lib:$LD_LIBRARY_PATH ${LINUXDEPLOY_DIR} \
     --appdir ${DEPLOY_DIR} \
     --output appimage \
